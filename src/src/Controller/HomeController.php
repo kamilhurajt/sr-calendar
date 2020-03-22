c8e55ce79637a5651e80c063e865970ca0b71e5a
Kamil Hurajt <hurajtk@gmail.com>
2020-03-22 18:09:45 +0100

    feature/framework-configuration: add default template & default home controller

---
+++ src/src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Sr\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
