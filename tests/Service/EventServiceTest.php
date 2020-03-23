904d451dcd9c92820e8e04adab55b0bd6a5341ee
Kamil Hurajt <hurajtk@gmail.com>
2020-03-23 13:30:17 +0100

    feature/events: add skeleton tests

---
+++ tests/Service/EventServiceTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+
+class EventServiceTest extends TestCase
+{
+    public function testCreate()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function testUpdate()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function testRemove()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function testGetEvents()
+    {
+        $this->assertTrue(false);
+    }
+}
---
+++ tests/Validation/Validator/EventValidatorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Validation\Validator;
+
+use PHPUnit\Framework\TestCase;
+
+class EventValidatorTest extends TestCase
+{
+    public function testValidate()
+    {
+        $this->assertTrue(false);
+    }
+}
